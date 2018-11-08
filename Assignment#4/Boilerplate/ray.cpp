a10b2f11104f27f5bf016b4683fd8cefeb022e8f
Benjamin Chen <benkun.chen@gmail.com>
2018-11-08 15:47:18 -0700

    u

---
+++ Assignment#4/Boilerplate/ray.cpp
@@ -0,0 +1,18 @@
+/*
+ * ray.cpp
+ *
+ *  Created on: Nov 8, 2018
+ *      Author: benkun.chen
+ */
+
+#include "ray.h"
+
+ray::ray() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ray::~ray() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Assignment#4/Boilerplate/ray.h
@@ -0,0 +1,20 @@
+/*
+ * ray.h
+ *
+ *  Created on: Nov 8, 2018
+ *      Author: benkun.chen
+ */
+
+#ifndef RAY_H_
+#define RAY_H_
+
+
+
+class ray {
+public:
+	ray();
+	virtual ~ray();
+
+};
+
+#endif /* RAY_H_ */
